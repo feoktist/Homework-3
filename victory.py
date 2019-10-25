8163c31c887cf5b1cb5459c46a42854fc1bc5125
Alexey Makarov <axel.makarov@gmail.com>
2019-10-25 13:55:57 +0300

    Update victory.py

--- victory.py
+++ victory.py
@@ -1,43 +1,45 @@
 import random
 
-questions = {'a': '01.01.2019', 'b': '02.02.2019', 'c': '03.03.2019', 'd': '04.04.2019', 'e': '05.05.2019',
-             'f': '06.06.2019', 'g': '07.07.2019', 'h': '08.08.2019', 'i': '09.09.2019', 'j': '10.10.2019'}
+questions = {'Alex': '01.01.2019', 'Bob': '02.02.2019', 'Charlie': '03.03.2019', 'Dave': '04.04.2019', 'Evan': '05.05.2019',
+             'Frank': '06.06.2019', 'Gregory': '07.07.2019', 'Helen': '08.08.2019', 'Ian': '09.09.2019', 'John': '10.10.2019'}
 
 people = questions.keys()
+
+
 
 say = input("Shall we play? ")
 
 while say not in {'n', 'N'}:
-    random_people = random.sample(people, 5)
-    counter = 0
-    errors = 0
-    print('\n')
-    print('Guess bdays of these guys: ', random_people)
-    print('\n')
+  random_people = random.sample(people, 5)
+  counter = 0
+  errors = 0
+  for k in random_people:
+      
+    
+    answer = input(f'Enter dd.mm.yyyy for writher {k}: ')
+    if answer == questions[k]:
+      print('Correct!')
+      print('\n')
+      counter += 1
+      
+      
+    else:
+      print('Wrong!')
+      print('The right answer is: ', questions[k])
+      print('\n')
+      errors += 1
+      
 
-    for writer in random_people:
-        print('----------------------------------')
-        print('Guess the dd.mm.yyyy for: ', writer)
-        print('\n')
-        answer = (input('your answer: '))
-        if answer in writers.values():
-            print('you guessed it right!')
-            counter = + 1
-        else:
-            print('wrong!')
-            print('the right answer is: ', writer.values())
-            errors = + 1
+  print('\n')
+  print(' =============== RESULTS ================')
+  print('Correct answers: ', counter)  
+  print('Wrong answers: ', errors)  
+  print(' =========================================')
 
-    print('Correct answers: ', counter)
-    print('Wrong answers: ', errors)
-
-    say = input("Shall we play again? (y/n): ")
+    
+  say = input("Shall we play again? (y/n): ")
 
 print('\n')
 print('Bye!')
 print('\n')
 print('End of program')
-
-
-
-
